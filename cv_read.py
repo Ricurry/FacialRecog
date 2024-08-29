27bc37bc6503eba4419ef042ee3ff16f6651bdd9
poet5 <154743932+Ricurry@users.noreply.github.com>
2024-08-28 19:49:32 -0500

    Add files via upload

--- cv_read.py
+++ cv_read.py
@@ -1,77 +1,82 @@
 import cv2 as cv
 import numpy as np
 import pyvirtualcam
+import pyautogui
+import ctypes
+from ctypes import wintypes 
+# Set failsafe to false
+pyautogui.FAILSAFE = False
+# Set the refresh of pyautogui to 0.01 seconds
+pyautogui.PAUSE = 0
 cam = cv.VideoCapture(0)
 #Set camera resolution to 256x256
+x = user32.GetSystemMetrics(0)
+y = user32.GetSystemMetrics(1)
+
 cam.set(3, 255)
 cam.set(4, 255)
-#Import pb file 'frozen_graph.pb' to opencv
 net = cv.dnn.readNetFromTensorflow('frozen_graph.pb')
 print("OpenCV model was successfully read. Model layers: \n", net.getLayerNames())
-#Define the classes
 classes = ['human', 'not_human']
 while(1):
     # Take each frame
     _, frame = cam.read()
+    # Frame should be rotated 90 degrees
+    frame = cv.rotate(frame, cv.ROTATE_90_CLOCKWISE)
     rows = frame.shape[0]
     cols = frame.shape[1]
     # Convert BGR to HSV
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    # define range of blue color in HSV
-    lower_blue = np.array([0, 20, 40])
-    upper_blue = np.array([20,255,255])
-    # Threshold the HSV image to get only blue colors
-    mask = cv.inRange(hsv, lower_blue, upper_blue)
-    res = cv.bitwise_and(frame,frame, mask= mask)
-    # Bitwise-AND mask and original image
+    # Upper and lower limits of the color of the hand
+    lower = np.array([0, 20, 40])
+    upper = np.array([20,255,255]) # color key
+    mask = cv.inRange(hsv, lower, upper) 
+    res = cv.bitwise_and(frame,frame, mask= mask) # using mask over the frame.
     cv.imshow('mask',mask)
     cv.imshow('frame', frame)
-    # Blob from image
-    blob = cv.dnn.blobFromImage(frame, size=(255, 255), swapRB=True, crop=False)   
-    net.setInput(blob)
-    cvOut = net.forward()
-    print(cvOut.shape   )
-    # Make box around 4 dimensional object
-    # Blob from image
-    blob = cv.dnn.blobFromImage(frame, size=(255, 255), swapRB=True, crop=False)   
-    net.setInput(blob)
-    cvOut = net.forward()
-
-    # Print information about cvOut
-    print("Shape of cvOut:", cvOut.shape)
+    blob = cv.dnn.blobFromImage(frame, size=(255, 255), swapRB=True, crop=False)  # Using the deep neural network to make a initial prediction.
+    net.setInput(blob) # Read from the tensorflow generated model the image.
+    cvOut = net.forward() # Go to next layer
+    print("Shape of cvOut:", cvOut.shape) 
     print("Contents of cvOut:")
     print(cvOut)
 
-# Extract information from the single detection
-    detection = cvOut[0, 0, 0, 0]
+
+    detection = cvOut
     score = cvOut
-    # Assuming threshold for considering as 'human'
     threshold = 0.05
 
-    # Assign class name based on the threshold
-    class_name = 'human' if score > threshold else 'not_human'
-# Process the detection if the score is above a threshold
-    print(score)
-    if score > 0.05:
-        # Directly use scalar values without indexing
-        class_index = int(detection)
-        bounding_size = detection * 5
-        # Center the bounding box around the center of the image
-        # The 255 is the size of the image
+    class_name = 'human' if score > threshold else 'not_human' # Checks the mean score and if its higher than the threshold it will use the the average positions of the x and y to move the mouse because we only need to make one prediction in order to lessen the use of AI. :3
+    if np.mean(mask) == 10:
+        pyautogui.click(duration=0.25)
 
-        center_x = int(bounding_size* cols)
-        center_y = int(bounding_size * rows)
+    if score > 0.05:
+        class_index = int(detection)
+        print(detection.shape)
+        bounding_size = detection * 3
+        center_x = int(bounding_size * cols * 1.5)
+        # bound sizes
+        center_y = int(bounding_size * rows * 1.5)
         width = int(bounding_size * cols)
         height = int(bounding_size * rows)
         topleft_x = int(center_x - width / 2)
         topleft_y = int(center_y - height / 2)
-        # Draw rectangle
-        cv.rectangle(frame, (topleft_x, topleft_y), (topleft_x + width, topleft_y + height), (0, 255, 0), thickness=2)
-        # Put text on image
+        bottomright_x = topleft_x + width
+        bottomright_y = topleft_y + height
+
+        cv.rectangle(frame, (topleft_x, topleft_y), (bottomright_x, bottomright_y), (0, 255, 0), thickness=2)
         cv.putText(frame, class_name, (topleft_x, topleft_y + 20), cv.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
         print("X position:", center_x)
         print("Y position:", center_y)
+        y_coords, x_coords = np.where(mask == 255)
 
+        average_x = np.mean(x_coords)
+        average_y = np.mean(y_coords)
+
+        # Move mouse to average x and y
+        # divides to position of monitor. We can use user32.getSystemMetrics(0) and user32.getSystemMetrics(1).
+        pyautogui.moveTo(average_x * x/500, average_y * y/500)
+ 
     cv.imshow('frame', frame)
 
     # Cvout is 2d array of 1x1x1x2
--- tf_human.py
+++ tf_human.py
@@ -12,33 +12,42 @@ from keras.models import load_model
 #GlobalAveragePooling2D
 from keras.layers import GlobalAveragePooling2D
 
-# Use NVIDIA CUDA GPU
-
-
-
+# YOU WILL DESERVE LOVE ONLY THROUGH YOUR WORK. NOTHING ELSE.
+# REMEMBER ZIA
+# REMEMBER ZIA
+# REMEMBER ZIA
+# REMEMBER ZIA
+# REMEMBER ZIA
+# REMEMBER ZIA
+# REMEMBER ZIA
+# REMEMBER ZIA
+# REMEMBER ZIA
+# REMEMBER ZIA
+# REMEMBER ZIA
+# REMEMBER ZIA
+# TRUST YOURSELF
+# TRUST NOBODY
+# WORK HARD
+# WORK SMART
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-
-
 # Use NVIDIA CUDA GPU
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 #Verify GPU is being used
-tf.config.experimental.list_physical_devices('GPU')
 # Lets begin!
 data_path = 'face_dataset'
+
+# Memory growth allows the GPU to use more memory than it normally would
+try:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+except:
+    # Invalid device or cannot modify virtual devices once initialized.
+    pass
+
 
 # # Of images.
 num_images = len(os.listdir(data_path))
 print("SYS_APOLLO ; Dataset Images : {an}".format(an = num_images))
-# Using PLT place 4 of the first images into 20x20 grid with 4 columns and provide labels   
-fig = plt.figure(figsize=(20,20))
-for i in range(4):
-    img = cv.imread(os.path.join(data_path +"/human", str(i)+'.png'))
-    # Make image normal rgb
-    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-    fig.add_subplot(1,4,i+1)
-    plt.imshow(img)
-    plt.title("Image " + str(i))
 
 
 # Define data as data folder in tf set label mode to categorical, set class names to human and not human
@@ -52,31 +61,33 @@ train = data.take(train_size)
 val = data.skip(train_size).take(val_size)
 test = data.skip(train_size+val_size).take(test_size)
 # Define the model
-model = Sequential()
+model = Sequential() # a sequential model is like a linear equation. so a constant rate of change. :3
 # Make output shape 255x255x3
 model.add(Conv2D(32, (3,3), activation='relu', input_shape=(256,256,3)))
+
+#convolution is like a treasure chest. It highlights the recurring patterns in a image :3.
+# and im going to touch you too, cuz you my treasure >:)
 
 # Add the first max pooling layer with a pool size of 2x2
 model.add(MaxPooling2D())
 # Add the second convolutional layer with 32 filters, 3x3 kernel size, relu activation
-model.add(Conv2D(32, (3,3), activation='relu'))
+model.add(Conv2D(32, (3,3), activation='relu')) # x in this case for relu is the input of the image, and so if it has a positive value then it will pass on hence (x + |x|)/2 ,  max (0,x) to make sure it is above 0. 
 # Add the second max pooling layer with a pool size of 2x2
 model.add(MaxPooling2D())
 # Add the third convolutional layer with 64 filters, 3x3 kernel size, relu activation
-model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(Conv2D(64, (3,3), activation='relu')) # 32 + 64 = 96
 # Add the third max pooling layer with a pool size of 2x2
 model.add(MaxPooling2D())
 
-model.add(Conv2D(1, (1,1), activation='sigmoid'))
+model.add(Conv2D(1, (1,1), activation='sigmoid')) # 96 + 1 = 97, sigmoid divides the value of significance so it is between 0 and 1.
 # Logits and labels must have the same shape so we do this to make it work and retain the shape
 model.add(GlobalAveragePooling2D())
 # But the above makes it 2d so we need to make it 4d again
 
-model.add(tf.keras.layers.Reshape((1,1,1)))
+model.add(tf.keras.layers.Reshape((1,1,1))) # Reshaping into 3d instead cuz im gangster >:) this wont work for opencv but i have a idea >:))
 # The above line averages out the data into a sign
 # Make output shape 253x253x3
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-# Print the model summary
 print(model.summary())
 
 logdir='logs'
@@ -86,7 +97,7 @@ tensorboard_callback = TensorBoard(log_dir=logdir)
 
 # Define the number of epochs
 
-hist = model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
+hist = model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback]) # epochs help make it more accurate basically the more epochs the more accurate it is. :3
 
 # Use tf.function to get best fit for X or model.
 full_model = tf.function(lambda x: model(x))
@@ -95,7 +106,8 @@ full_model = full_model.get_concrete_function(tf.TensorSpec(model.inputs[0].shap
 # What is a frozen model? Frozen model is a model that optimizes the model for usage
 # In most areas where some layers cant be interpreted by the model.
 #Such as openCV.
-
+# Include the function to convert variables to constants
+from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
 
 # To do these we convert all the variables into constants,
 # Const variables are easier to process than normal dynamic variables because they cant be changed as easily.
@@ -109,7 +121,8 @@ frozen_func.graph.as_graph_def()
 
 tf.io.write_graph(graph_or_graph_def=frozen_func.graph,
                     logdir=".",
-                    name="frozen_graph.pb",
+                    name="frozen_graph2.pb",
                     as_text=False)
 
+print(model.output_shape)
 # And we are done! Now we can use the model in opencv.
